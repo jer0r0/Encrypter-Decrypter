dc754fbeb98ebd80cf35187b128b5f8ba506bfab
Jeroro <97175757+jer0r0@users.noreply.github.com>
2024-02-03 23:43:03 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -20,12 +20,19 @@ function mostrarAviso(mensaje,duracion) {
       aviso.style.display = "none";
     }, duracion);
   }
+console.log(screen.width)
+//establecer condiciones iniciales 
+if(screen.width > 1100){
+document.getElementById('outputText').style.background='no-repeat center/80% url(images/buscando.png)';
+}
 
- 
 //cambiar los placeholders 
 
 document.getElementById("outputText").placeholder = "Ningún mensaje fue encontrado";
 document.getElementById("inputText").placeholder = "Ingrese el texto aquí";
+
+//cambiar valor alerta de minusculas y mayusculas 
+document.getElementById("alertaChar").textContent = 'Solo letras minusculas. Sin acentos y/o caracteres especiales.';
 
 //Listener del boton de encriptar
 
@@ -35,18 +42,68 @@ document.getElementById('encryptButton').addEventListener('click', function () {
     if(inputText == ''){
         mostrarAviso('Por favor, ingresa texto antes de encriptar.',800);
         var encryptedText = encryptText(inputText);
+        document.getElementById('copyButton').style.visibility='hidden';
+        document.getElementById('outputText').placeholder = "Ningún mensaje fue encontrado";
+        if(screen.width > 1100){
+            document.getElementById('outputText').style.background='no-repeat center/80% url(images/buscando.png)';
+        }else{
+            document.getElementById('outputText').style.background='white';
+        }
+    }else{
+        document.getElementById('copyButton').style.visibility='visible';
+        document.getElementById('outputText').style.background='white';
     }
+    
     var encryptedText = encryptText(inputText);
     document.getElementById('outputText').value = encryptedText;
 });
+
+//Listener del keypress del input text de encriptar
+
+document.getElementById('inputText').addEventListener('keyup', function () {
+    var inputText = document.getElementById('inputText').value;
+    validateText(inputText);
+    if(inputText == ''){
+        //mostrarAviso('Por favor, ingresa texto antes de encriptar.',800);
+        var encryptedText = encryptText(inputText);
+        document.getElementById('copyButton').style.visibility='hidden';
+        document.getElementById('outputText').placeholder = "Ningún mensaje fue encontrado";
+        if(screen.width > 1100){
+            document.getElementById('outputText').style.background='no-repeat center/80% url(images/buscando.png)';
+        }else{
+            document.getElementById('outputText').style.background='white';
+        }
+    }else{
+        document.getElementById('copyButton').style.visibility='visible';
+        document.getElementById('outputText').style.background='white';
+    }
+    
+    var encryptedText = encryptText(inputText);
+    document.getElementById('outputText').value = encryptedText;
+});
+
+
+
+
 
 //Listener del boton de desencriptar 
 
 document.getElementById('decryptButton').addEventListener('click', function () {
     var encryptedText = document.getElementById('inputText').value;
+    validateText(encryptedText);
     if(encryptedText == ''){
         mostrarAviso('Por favor, ingresa texto encriptado antes de desencriptar.',800);
-        
+        var decryptedText = decryptText(encryptedText);
+        document.getElementById('copyButton').style.visibility='hidden';
+        document.getElementById('outputText').placeholder = "Ningún mensaje fue encontrado";
+        if(screen.width > 1100){
+            document.getElementById('outputText').style.background='no-repeat center/80% url(images/buscando.png)';
+        }else{
+            document.getElementById('outputText').style.background='white';
+        }
+    }else{
+        document.getElementById('copyButton').style.visibility='visible';
+        document.getElementById('outputText').style.background='white';
     }
     var decryptedText = decryptText(encryptedText);
     document.getElementById('outputText').value = decryptedText;
@@ -86,27 +143,45 @@ function validateText(text){
     if(containsSpecialChars || containsSpecialCharacters(text)){
         console.log('No se permiten acentos ni caracteres especiales.');
         mostrarAviso('El texto tiene caracteres especiales ', 2000);
-        document.getElementById("outputText").placeholder = "El texto suministrado no cumple con las especificaiones.";
+        document.getElementById('outputText').placeholder = "El texto suministrado no cumple con las especificaciones";
+        document.getElementById('copyButton').style.visibility='hidden';
+        if(screen.width > 1100){
+            document.getElementById('outputText').style.background='no-repeat center/80% url(images/SenorError.png)';
+        }else{
+            document.getElementById('outputText').style.background='white';
+        }
+        /*
         setTimeout(() => {
             document.getElementById("outputText").placeholder = "Ningún mensaje fue encontrado";
+            document.getElementById('outputText').style.background='no-repeat center/80% url(images/buscando.png)';
             //document.getElementById('inputText').value = "";
-        }, 2000);
+        }, 2000);*/
         return false;
     }else if(containsUpperCase){
         console.log('No se permiten letras Mayusculas.');
         mostrarAviso('El texto contiene letras Mayusculas.', 2000);
-        document.getElementById("outputText").placeholder = "El texto suministrado no cumple con las especificaiones.";
+        document.getElementById('copyButton').style.visibility='hidden';
+        document.getElementById('outputText').style.background='no-repeat center/80% url(images/SenorError.png)';
+        if(screen.width > 1100){
+            document.getElementById('outputText').style.background='no-repeat center/80% url(images/SenorError.png)';
+        }else{
+            document.getElementById('outputText').style.background='white';
+        }
+        /*
         setTimeout(() => {
-            document.getElementById("outputText").placeholder = "Ningún mensaje fue encontrado";
+            document.getElementById('outputText').placeholder = "Ningún mensaje fue encontrado";
+            document.getElementById('outputText').style.background='no-repeat center/80% url(images/buscando.png)';
             //document.getElementById('inputText').value = "";
         }, 2000);
+        */
         return false;
     }else{
-        console.log("No los contiene");
+        //console.log("No los contiene");
         return true;
     }
 
 }
+
 
 function encryptText(text) {
     
@@ -131,19 +206,37 @@ function finallyEncrypt(inputText){
 }
 
 function finallyDecrypt(inputText){
-    const result = inputText
-    .replace(/enter/g,'e')
-    .replace(/imes/g,'i')
-    .replace(/ai/g,'a')
-    .replace(/ober/g,'o')
-    .replace(/ufat/g,'u')
-    ;
-
-    return result;
+    let finalText = "";
+    let vowels = {
+        "a": 1,
+        "e": 4,
+        "i": 3,
+        "o": 3,
+        "u": 3
+    }
+    for (let i = 0; i < inputText.length; i++) {
+        let skipCount = vowels[inputText[i]];
+        
+        let currentCharacter = inputText[i];
+        console.log(currentCharacter)
+        if (/^[a-z]$/.test(currentCharacter)) {    
+            if (skipCount !== undefined) {
+                finalText += currentCharacter;
+                i += skipCount;
+            } else {
+                finalText += currentCharacter;
+            }
+        }
+    }
+    return finalText;
 }
 function decryptText(encryptedText) {
-
-    return finallyDecrypt(encryptedText);
+    if(validateText(encryptedText)){
+        const result = finallyDecrypt(encryptedText);
+        return result;
+    }else{
+        return '';
+    }
     /*return atob(encryptedText);*/
 }
 
